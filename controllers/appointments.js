78ed9e5820c59ff50daf0787602b8caea5ad5b25
Day Little <dayana.y.little@gmail.com>
2023-01-17 09:59:59 -0500

    write show functionality

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -13,6 +13,45 @@ function index(req, res) {
     res.redirect("/")
   })
 }
+
+function create(req, res) {
+  req.body.owner = req.user.profile._id
+	req.body.flexible = !!req.body.flexible
+  Appointment.create(req.body)
+  .then(appointment => {
+    res.redirect('/appointments')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/appointments')
+  })
+}
+
+function show(req, res) {
+  Appointment.findById(req.params.id)
+  .populate("owner")
+  .then(appointment => {
+    res.render('appointments/show', {
+      taco,
+      title: "View Appointments"
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/appointnments')
+  })
+}
+
+
+
+
+
+
+
+
+
 export {
-  index
+  index,
+  create,
+  show,
 }
--- routes/appointments.js
+++ routes/appointments.js
@@ -1,9 +1,12 @@
 import { Router } from 'express'
 import * as appointmentsCtrl from '../controllers/appointments.js'
+import { isLoggedIn } from '../middleware/middleware.js'
+
 const router = Router()
 
 router.get('/', appointmentsCtrl.index)
-
+router.post('/', isLoggedIn, appointmentsCtrl.create)
+router.get('/:id', appointmentsCtrl.show)
 export {
   router
 }
