872fc89fd29f1eac33a1f6ea16149e2c18cf4435
Day Little <dayana.y.little@gmail.com>
2023-01-25 22:12:13 -0500

    fix broken date code

--- models/review.js
+++ models/review.js
@@ -6,7 +6,7 @@ const Schema = mongoose.Schema
 const reviewSchema = new Schema({
   author: {type: String, required: true},
   owner: {type: Schema.Types.ObjectId, ref: "Profile"},
-  createdAt: String,
+  service: {type: String, required: true},
   content: {type: String, required: true},
   serviceDate: String,
   rating: {type: Number, min: 1, max: 5, default: 5}
